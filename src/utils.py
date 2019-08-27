a58b1e96fa556f3cb07f048eb0a6f8843b2f20cb
Daniel Bailo <danbailoufms@gmail.com>
2019-08-27 14:59:08 -0400

    codigo mais otimizado

--- src/utils.py
+++ src/utils.py
@@ -3,42 +3,44 @@ import time
 import core
 
 def get_instances(directory):
-	return [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+	return sorted(os.listdir(directory), key=lambda k:int(k.strip('input.in')))
+
+def make_test(function,number_items, weight_max, values_items, weight_items):
+	start = time.time()
+	answer = function(number_items, weight_max, values_items, weight_items)
+	t = time.time()-start
+	return answer, t
 
 def get_results(directory):	
-	all_results_decrescent = []
-	all_results_crescent = []
-	all_results_efficiency = []
 
-	instance = []
+	decrescent = []
+	crescent = []
+	efficiency = []
+
 	for input_file in get_instances(directory): 
-		with open(directory+input_file) as file: 
-			for line in file: instance.append(line.split())
-		state = 0
-		weight_items = []
-		values_items = []
-		for inst in instance:
-			if state == 0:
-				number_items = int(inst[0])
-				state = 1
-			elif state == 1:			
-				values_items.append(int(inst[1]))
-				weight_items.append(int(inst[2]))
-				item_id = int(inst[0])
-				if item_id == number_items: state = 2
-			elif state == 2:
-				weight_max = int(inst[0])
+		with open(os.path.join(directory,input_file)) as file: 
+			state = 0
+			weight_items = []
+			values_items = []
+			for line in file: 
+				inst = line.split()
+				if state == 0:
+					number_items = int(inst[0])
+					state = 1
+				elif state == 1:
+					item_id = int(inst[0])
+					values_items.append(int(inst[1]))
+					weight_items.append(int(inst[2]))
+					if item_id == number_items: state = 2
+				elif state == 2:
+					weight_max = int(inst[0])
+			decrescent.append((input_file,make_test(core.decrescent,number_items, weight_max, values_items, weight_items)))
+			crescent.append((input_file,make_test(core.crescent,number_items, weight_max, values_items, weight_items)))
+			efficiency.append((input_file,make_test(core.efficiency,number_items, weight_max, values_items, weight_items)))	
+	
+	return decrescent, crescent, efficiency
 
-		start = time.time()
-		result_decrescent = core.decrescent(number_items, weight_max, values_items, weight_items)
-		all_results_decrescent.append((input_file,result_decrescent, '{0:.5}'.format(time.time()-start)))
-		
-		start = time.time()
-		result_crescent = core.crescent(number_items, weight_max, values_items, weight_items)
-		all_results_crescent.append((input_file,result_crescent, '{0:.5}'.format(time.time()-start)))
-
-		start = time.time()
-		result_efficiency = core.efficiency(number_items, weight_max, values_items, weight_items)
-		all_results_efficiency.append((input_file,result_efficiency, '{0:.5}'.format(time.time()-start)))
-		instance.clear()
-	return all_results_decrescent, all_results_crescent, all_results_efficiency
+#debugger
+# if __name__ == "__main__":
+# 	directory = '../inputs/'
+# 	print(get_results(directory))
