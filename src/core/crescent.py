0fd51bea8247460905495a4b752ffa4475778620
Daniel Bailo <danbailoufms@gmail.com>
2019-08-28 19:03:26 -0400

    arrumei as heuristicas, vou modificar a formacao delas agr

--- src/core/crescent.py
+++ src/core/crescent.py
@@ -1,11 +1,12 @@
-def crescent(number_items, weight_max, values_items, weight_items):
+def crescent(weight_max, values_items, weight_items):
 	"""Knapsack guloso, ordena de forma crescente os valores e os pesos dos items"""
-	values_items.sort(reverse=False)
-	weight_items.sort(reverse=False)
-	k = []
-	result = []
-	for i in range(number_items):
-		if weight_items[i]<weight_max and sum(k) < weight_max:
-			k.append(weight_items[i])
-			result.append(values_items[i])
-	return sum(result)	
+	items = dict(zip(values_items, weight_items))
+	items = sorted(items.items())
+	result_final = []
+	weight = []
+
+	for values_items,weight_items in items:
+		if weight_items+sum(weight) < weight_max and weight_items < weight_max: 
+			result_final.append(values_items)
+			weight.append(weight_items)	
+	return sum(result_final)
--- src/core/efficiency.py
+++ src/core/efficiency.py
@@ -1,19 +1,24 @@
 import numpy as np
 
-def efficiency(number_items,weight_max, values_items, weight_items):
+def efficiency(number_items, weight_max, values_items, weight_items):
 	"""Knapsack guloso, utiliza a eficiência, a partir da razão "valor/peso" para selecionar os itens"""
 	value_efficiency = np.array(values_items)
 	weight_items_efficiency = np.array(weight_items)
 	efficiency = value_efficiency/weight_items_efficiency
 	efficiency = list(efficiency)
+	
+	items = {}
 
-	k = []
-	result = []
-	while True:
-		index = efficiency.index(max(efficiency))
-		if weight_items[index]<weight_max and sum(k)<weight_max:
-			efficiency.pop(index)
-			k.append(weight_items.pop(index))
-			result.append(values_items.pop(index))
-		else: break			
-	return sum(result)	
+	for i in range(number_items): 
+		items[efficiency[i]] = values_items[i],weight_items[i]
+
+	items = sorted(items.items(), reverse=True)
+
+	result_final = []
+	weight = []
+
+	for _,result in items:
+		if result[1]+sum(weight) < weight_max and result[1] < weight_max: 
+			result_final.append(result[0])
+			weight.append(result[1])		
+	return sum(result_final)	
