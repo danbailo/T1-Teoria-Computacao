fe92c974792c14dd3fb99460fc67334fc1060794
Daniel Bailo <danbailoufms@gmail.com>
2019-09-21 22:22:43 -0400

    mais otimizado, mas acho q da pra melhorar

--- src/core/grasp.py
+++ src/core/grasp.py
@@ -1,6 +1,8 @@
 import numpy as np
 import random
 
+random.seed(42)
+
 def semi_greedy_construction(window, number_items, weight_max, values_items, weight_items):
 	efficiency = np.divide(values_items, weight_items)
 	items = {}
@@ -9,6 +11,7 @@ def semi_greedy_construction(window, number_items, weight_max, values_items, wei
 	items = sorted(items.values(), reverse=True)
 
 	result_final = []
+	value = 0
 	weight = 0
 
 	aux = items[:]
@@ -17,28 +20,22 @@ def semi_greedy_construction(window, number_items, weight_max, values_items, wei
 		else: tmp_window = len(items)
 		
 		index = random.randint(0,tmp_window-1)
+		value_item = items[index][1]
 		weight_item = items[index][2]
 		
 		if weight_item+weight <= weight_max: 
 			result_final.append(items.pop(index))
+			value += value_item
 			weight += weight_item
 		else: items.pop(index)
 
 	solution = [0 for i in range(number_items)]
 
-	for value in aux:
-		if value in result_final: solution[aux.index(value)] = 1
-	return solution, aux
+	for aux_value in aux:
+		if aux_value in result_final: solution[aux.index(aux_value)] = 1
+	return solution, aux, value
 
-def local_search(solution, aux, weight_max):
-	default_value = 0
-	default_weight = 0
-
-	for i in range(len(solution)):
-		if solution[i] == 1:
-			default_value += aux[i][1]
-			default_weight += aux[i][2]
-
+def local_search(solution, aux, value, weight_max):
 	all_solutions = []
 	solution_aux = solution[:]
 	for i in range(len(solution)):
@@ -49,28 +46,27 @@ def local_search(solution, aux, weight_max):
 
 	new_solution = solution_aux[:]
 	for solution in all_solutions:
-		value = 0
-		weight = 0		
+		new_value = 0
+		new_weight = 0		
 		for j in range(len(solution)):
 			if solution[j] == 1:
-				value += aux[j][1]
-				weight += aux[j][2]
-		if weight <= weight_max and value > default_value:
+				new_value += aux[j][1]
+				new_weight += aux[j][2]
+		if new_weight <= weight_max and new_value > value:
 			new_solution = solution[:]
-			default_value = value
+			value = new_value
 
 	if new_solution == solution_aux: 
-		return default_value
-	return local_search(new_solution,aux,weight_max)
+		return value
+	return local_search(solution, aux, value, weight_max)
 
-def grasp(max_it, seed, window, number_items, weight_max, values_items, weight_items):
+def grasp(max_it, window, number_items, weight_max, values_items, weight_items):
 	best_solution = 0
-	random.seed(seed)
 	for i in range(max_it):
-		solution, aux = semi_greedy_construction(window, number_items, weight_max, values_items, weight_items)
-		solution = local_search(solution, aux, weight_max)
+		solution, aux, value = semi_greedy_construction(window, number_items, weight_max, values_items, weight_items)
+		solution = local_search(solution, aux, value, weight_max)
+		print(solution)
 		if solution > best_solution: best_solution = solution
 	return best_solution			
 
 
-	
