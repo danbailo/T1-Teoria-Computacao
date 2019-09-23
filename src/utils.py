6593b5b99a4e0dd78dd2ce8ea2d4e66d46342233
Daniel Bailo <danbailoufms@gmail.com>
2019-09-23 12:53:01 -0400

    modificando o grasp

--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,16 @@
 import os
 from time import time
 import json
+from collections import defaultdict
 import core
 
-max_it = 100
-window = 2
+# max_it = 100
+# window = 2
 
 hooks = [
 	('Crescente', core.crescent),
 	('Decrescente', core.decrescent),
 	('Eficiente', core.efficiency),
-	('GRASP', lambda number_items, weight_max, values_items, weight_items :core.grasp(max_it, window, number_items, weight_max, values_items, weight_items)),
 	('Exato', core.exact)
 ]
 
@@ -55,6 +55,41 @@ def get_results(directory):
 				time_results[input_file][name] = time() - start
 	with open(os.path.join('..','results','result.json'),'w') as file: file.write(json.dumps(result,indent=4))
 	with open(os.path.join('..','results','time.json'),'w') as file: file.write(json.dumps(time_results,indent=4))
+
+def get_GRASP_results(directory):	
+	result = {}
+	time_results = {}
+
+	windows = [2,3,4,5,6,7,8,9]
+	iters = [10,100,1000,10000]
+
+	for input_file in get_instances(directory): 
+		with open(os.path.join('..',directory,input_file)) as file: 
+			state = 0
+			weight_items = []
+			values_items = []
+			for line in file: 
+				inst = line.split()
+				if state == 0:
+					number_items = int(inst[0])
+					state = 1
+				elif state == 1:
+					item_id = int(inst[0])
+					values_items.append(int(inst[1]))
+					weight_items.append(int(inst[2]))
+					if item_id == number_items: state = 2
+				elif state == 2:
+					weight_max = int(inst[0])
+			result[input_file] = {}
+			for max_it in iters:
+				for window in windows:
+					start = time()
+					result[input_file]["MAX_IT-"+str(max_it)]["Window-"+str(window)] = core.grasp(max_it, window, number_items, weight_max, values_items, weight_items)
+					time_results[input_file]["MAX_IT"-+str(max_it)]["Window-"+str(window)] = time() - start
+
+	with open(os.path.join('..','results','result_GRASP.json'),'w') as file: file.write(json.dumps(result,indent=4))
+	with open(os.path.join('..','results','time_GRASP.json'),'w') as file: file.write(json.dumps(time_results,indent=4))
+
 
 #debugger
 # if __name__ == "__main__":
